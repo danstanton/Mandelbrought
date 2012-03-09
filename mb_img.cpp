a3f6df35b21867922099815a4ae81c3ebcf9e70a
Daniel Stanton <geometry180@gmail.com>
2012-03-08 22:07:56 -0600

    began implementing gradient usage

--- mb_img.cpp
+++ mb_img.cpp
@@ -2,6 +2,7 @@
 #include <cstdio>
 #include <cstdlib>
 #include <string>
+#include <utility> // for pair
 #include <gmp.h>
 #include <png.h>
 
@@ -10,6 +11,38 @@
 using namespace std;
 
 typedef unsigned int uint;
+
+struct color_node {
+	char red, green, blue;
+	color_node *next;
+};
+
+enum fn_type {LINEAR};
+
+struct RGB {
+	char elem[3];
+};
+
+RGB linear_mix( int current_position, int total_distance, RGB first, RGB second) {
+	double mix_strength = double(current_position)/double(total_distance);
+	RGB result;
+	
+	for(uint i=0; i<3; i++)
+		result.elem[i] = linear_helper(mix_strength, first.elem[i], second.elem[i]);
+	return result;
+};
+
+char linear_helper(double strength, char first, char second) {
+	return (char)((1-strength)*first + strength*second);
+};
+
+pair<string,RGB (*)(int, int, RGB, RGB)> fns = {pair("linear",&linear_mix)};
+
+struct fn_node {
+	fn_type type;
+	uint distance;
+	fn_node *next;
+};
 
 /* This is a simple wrapper around an error condition. It will print
  * a very simple error message. */
@@ -24,8 +57,8 @@ bool flag_eof( int error, string task) {
 };
 
 int main( int ac, char ** av) {
-	if ( ac < 2 ) {
-		printf("Usage: %s dat_file \n", av[0]);
+	if ( ac < 3 ) {
+		printf("Usage: %s dat_file color_file\n", av[0]);
 		return 0;
 	}
 
@@ -33,19 +66,25 @@ int main( int ac, char ** av) {
 
 	for(uint i = 0; i < strlen(av[1])-4; i++)
 		filename.push_back(av[1][i]);
+	filename.push_back('-');
+	for(uint i = 0; i < strlen(av[2])-5; i++)
+		filename.push_back(av[2][i]);
 	filename.append(".png");
 
 	printf("%s\n",filename.c_str());
 
 	FILE *in_file=fopen(av[1], "r"),
+		 *color_file=fopen(av[2], "r"),
 		 *out_file=fopen(filename.c_str(), "wb");
 
 	png_structp png_ptr = NULL;
 	png_infop info_ptr = NULL;
 	png_bytep *img_row = NULL;
 
+
 	bool err_jump = false;
 	err_jump |= flag_error(!in_file, "open data file");
+	err_jump |= flag_error(!color_file, "open gradient file");
 	err_jump |= flag_error(!out_file, "ready image output file");
 	if(err_jump) goto cleanup;
 
@@ -61,6 +100,22 @@ int main( int ac, char ** av) {
 	uint img_width, img_height; // <-- Things to get from the data file
 	if(flag_eof(fscanf(in_file, "%ux%u", &img_width, &img_height), "read resolution"))
 		goto cleanup;
+
+	// vvvvv  Things to get from the color file
+	color_node *head_color = new color_node, *temp_color = NULL;
+	fn_node *head_fn = new fn_node, *temp_fn = NULL;
+	head_color.next = NULL;
+	head_fn.next = NULL;
+	char *temp_read=NULL;
+
+	fscanf(color_file, "%s", temp_read);
+
+	if(flerr(!parseHex(temp_read), "parse color spec"))
+		goto cleanup;
+
+	
+	
+	
 
 	// Initialize the PNG structure for writing
 	if(flag_error(setjmp(png_jmpbuf(png_ptr)), "initialize PNG I/O")) 
@@ -108,6 +163,7 @@ int main( int ac, char ** av) {
 
 cleanup:
 	if(in_file) fclose(in_file);
+	if(color_file) fclose(color_file);
 	if(out_file) fclose(out_file);
 	if(png_ptr) png_destroy_write_struct(&png_ptr, &info_ptr);
 	if(img_row) {
