7392b1aa05b983203535e0d3cb61a80f91c0ce98
Daniel Stanton <geometry180@gmail.com>
2012-03-23 19:08:00 -0500

    Prepared all source files for release under GPLv3.

--- path_finder.cpp
+++ path_finder.cpp
@@ -1,3 +1,20 @@
+/* Copyright 2012 Daniel Jacob Stanton
+This file is part of Mandelbrought
+
+Mandelbrought is free software: you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation, either version 3 of the License, or (at your
+option) any later version.
+
+Mandelbrought is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with Mandelbrought (in COPYING). If not, see 
+<http://www.gnu.org/licenses/>.    */
+
 #include "fractal_image.h"
 
 using namespace std;
@@ -23,7 +40,7 @@ void Fractal_image::step(int &x, int &y, int x_direction, int y_direction) {
 }
 
 bool Fractal_image::flat_area(int x, int y, int depth, bool side) {
-	return (in_image(x, y) && (get_depth(x, y) == depth) && (above_axis[y][x] == side));
+	return (in_image(x, y) && (get_depth(x, y) == depth)); // && (above_axis[y][x] == side));
 }
 
 void Fractal_image::fill_area(int x, int y) {
